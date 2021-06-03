b59dd3e9a18e25acf9966cefe24d863064466ba6
yuying <812334446@qq.com>
2021-06-03 21:30:30 +0800

    中期. Ref #10

---
+++ app/src/main/java/com/example/water11/ui/EditActivity.java
@@ -0,0 +1,15 @@
+package com.example.water11.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.water11.R;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
--- app/src/main/java/com/example/water11/ui/Service/knowledge/KnowledgeFragment.java
+++ app/src/main/java/com/example/water11/ui/Service/knowledge/KnowledgeFragment.java
@@ -39,9 +39,13 @@ public class KnowledgeFragment extends Fragment {
     }
 
     public void initKnowledge(){
-        Knowledge knowledge1=new Knowledge("2020年全国科普日来了！","   2020年全国科普日活动将于9月19日至25日在全国各地集中开展...","https://baijiahao.baidu.com/s?id=1678247188005126351&wfr=spider&for=pc",R.drawable.knowledge1,0);
+        Knowledge knowledge1=new Knowledge("2021年水利政策法规工作，17项要点值得关注","   2021年水利政策法规工作要坚持以习近平新时代中国特色社会主义思想为指导，深入贯彻落实党的十九届二中、三中、四中、五中全会精神…","https://www.thepaper.cn/newsDetail_forward_12175742",R.drawable.knowledge1,0);
         knowledgeList.add(knowledge1);
         Knowledge knowledge2=new Knowledge("3.22世界水日","   收藏学习！这些节水知识你都了解吗？...","https://baijiahao.baidu.com/s?id=1694897715885372627&wfr=spider&for=pc",R.drawable.knowledge2,0);
         knowledgeList.add(knowledge2);
+        Knowledge knowledge3=new Knowledge("涨知识啦！！！","   细数历年“世界水日”主题和“中国水周”宣传主题","https://m.thepaper.cn/baijiahao_11822832",R.drawable.knowledge3,0);
+        knowledgeList.add(knowledge3);
+        Knowledge knowledge4=new Knowledge("新鲜出炉！2021年《世界水发展报告》","   与大多数其他珍贵的资源不同，水的真正“价值”很难被确定, 这一关键资源的总体重要性在世界许多地区的政治关注和财政投资中没有得到充分体现…","https://www.thepaper.cn/newsDetail_forward_11956746",R.drawable.knowledge4,0);
+        knowledgeList.add(knowledge4);
     }
 }
